34dab449bbf2199e7d95bdeec1d216d97be915db
Thupten N Chakrishar <vajradog@users.noreply.github.com>
2025-08-25 05:41:43 -0400

    Add gamchung mapping to dispatch

--- api/dispatch.js
+++ api/dispatch.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
     const map = {
       chakrishar: 'vajradog/chakrishar-blog',
-      // add more as you create them:
+      /gamchung: 'vajradog/gamchung-blog',
       // gamchung: 'vajradog/gamchung-blog',
       // tibetancalligraphy: 'vajradog/tibetancalligraphy-blog',
     }
